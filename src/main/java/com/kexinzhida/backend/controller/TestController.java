6d47d44d96dd4923f95f8fb375af7bdd53a5c916
严威 <yanwei@hn-zhixin.com>
2022-03-31 10:39:36 +0800

    初始化代码格式

--- src/main/java/com/kexinzhida/backend/controller/TestController.java
+++ src/main/java/com/kexinzhida/backend/controller/TestController.java
@@ -5,4 +5,8 @@ package com.kexinzhida.backend.controller;
  * @since :  2022/3/30 21:22
  */
 public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
 }
